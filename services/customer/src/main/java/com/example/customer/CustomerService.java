dab83fda1fa91a5c9791ce233b7283ad0e9f9bde
Vladislav <vladislav.bronzov@gmail.com>
2023-11-04 12:44:37 +0100

    add customer service and base settings for the project

---
+++ services/customer/src/main/java/com/example/customer/CustomerService.java
@@ -0,0 +1,17 @@
+package com.example.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository)
+{
+    public void register(CustomerRegistrationRequest customerRequest)
+    {
+        Customer customer = Customer.builder()
+                .firstName(customerRequest.firstName())
+                .lastName(customerRequest.lastName())
+                .email(customerRequest.email()).build();
+
+        customerRepository.save(customer);
+    }
+}
